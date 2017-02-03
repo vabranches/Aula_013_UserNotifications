1342dfc82070851b3367c86cb162ba8822b096ae
Swift <swift@valter.local>
2017-02-02 22:02:05 -0200

    ok

--- Aula_013_UserNotifications/ViewController.swift
+++ Aula_013_UserNotifications/ViewController.swift
@@ -1,25 +1,81 @@
-//
-//  ViewController.swift
-//  Aula_013_UserNotifications
-//
-//  Created by Swift on 02/02/17.
-//  Copyright © 2017 Swift. All rights reserved.
-//
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
+    //MARK: Outlets
+    @IBOutlet weak var meuDatePicker: UIDatePicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Autorizacao para utilizar notificacoes
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (aceito, erro) in
+            if !aceito {
+                print("Acesso a notificações negado pelo usuário")
+            }
+        }
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    @IBAction func mudarData(_ sender: UIDatePicker) {
+        let dataSelecionada = sender.date
+        dispararNotificacao(na: dataSelecionada)
     }
 
+    //MARK: Metodos Personalizados
+    func dispararNotificacao(na data : Date) {
+        
+        //Gatinho de escolha da data
+        //let gatilho = UNTimeIntervalNotificationTrigger //Por tempo
+        let gatilho = UNCalendarNotificationTrigger(dateMatching: data.paraDateComponents, repeats: false) //Por data
+        
+        //Conteudo da notificacao
+        let conteudo = UNMutableNotificationContent()
+        conteudo.title = "Notificação do Calendário"
+        conteudo.body = "Notificação local agendada."
+        conteudo.sound = UNNotificationSound.default()
+        conteudo.categoryIdentifier = "minhaCategoria"
+        
+        //Botão da notificação
+        let acaoRelembrar = UNNotificationAction(identifier: "relembrar", title: "Me lembre mais tarde", options: [])
+        let categoria = UNNotificationCategory(identifier: "minhaCategoria", actions: [acaoRelembrar], intentIdentifiers: [], options: [])
+        
+        UNUserNotificationCenter.current().setNotificationCategories([categoria])
+        
+        //Requisiçao de apresentacao da notificacao
+        let requisicao = UNNotificationRequest(identifier: "textoNotificacao", content: conteudo, trigger: gatilho)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests() //Remove as notificacoes anteriores
+        
+        UNUserNotificationCenter.current().add(requisicao, withCompletionHandler: nil)
+        
+    }
+    
+    //MARK: Metodos de UNUserNotificationCenterDelegate
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.actionIdentifier == "relembrar" {
+            let novaData = Date(timeInterval: 61.0, since: Date())
+            dispararNotificacao(na: novaData)
+        }
+    }
 
 }
 
+
+//MARK: Extensao da classe Date para converter em DateComponents
+
+extension Date{
+    var paraDateComponents : DateComponents {
+        let calendario = Calendar(identifier: .gregorian)
+        let componentes = calendario.dateComponents(in: .current, from: self)
+        let novosComponentes = DateComponents(calendar: calendario, timeZone: .current, month: componentes.month, day: componentes.day, hour: componentes.hour, minute: componentes.minute)
+        return novosComponentes
+    }
+}
